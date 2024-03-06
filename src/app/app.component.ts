e0185a864919318a28f39d504581acf2bf03b716
Gopesh Sharma <gopesh.sharma@gmail.com>
2024-03-05 21:12:16 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'base-angular-app';
+  title = 'base-angular-app-with-bootstrap';
 }
